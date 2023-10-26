58f6ce1d90501ff08bde8e9b18f98783acb0dbc2
krystekw2 <waleckrystian2@gmail.com>
2023-10-26 09:49:55 +0200

    Initial commit

---
+++ FileClealer/FormDesktop.Designer.cs
@@ -0,0 +1,228 @@
+﻿
+namespace FileClealer
+{
+    partial class FormDesktop
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbListToDelete = new System.Windows.Forms.ComboBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.oFD1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnChooseFiles = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnReplace = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.cbListToDelete, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox1, 0, 4);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 27F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 37F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(299, 270);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(293, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Lista wartości usuwanych:";
+            // 
+            // cbListToDelete
+            // 
+            this.cbListToDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbListToDelete.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbListToDelete.FormattingEnabled = true;
+            this.cbListToDelete.Location = new System.Drawing.Point(3, 35);
+            this.cbListToDelete.Name = "cbListToDelete";
+            this.cbListToDelete.Size = new System.Drawing.Size(293, 21);
+            this.cbListToDelete.TabIndex = 2;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.btnAdd, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.btnDelete, 1, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 67);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(293, 34);
+            this.tableLayoutPanel2.TabIndex = 5;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAdd.Location = new System.Drawing.Point(3, 3);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(140, 28);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Dodaj";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAddItem_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDelete.Location = new System.Drawing.Point(149, 3);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(141, 28);
+            this.btnDelete.TabIndex = 1;
+            this.btnDelete.Text = "Usuń wybrane";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnRemoveItem_Click);
+            // 
+            // oFD1
+            // 
+            this.oFD1.Multiselect = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(3, 127);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(293, 140);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Usuwanie z plików";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnChooseFiles);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btnReplace);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 16);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(287, 121);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnChooseFiles
+            // 
+            this.btnChooseFiles.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnChooseFiles.Location = new System.Drawing.Point(6, 9);
+            this.btnChooseFiles.Name = "btnChooseFiles";
+            this.btnChooseFiles.Size = new System.Drawing.Size(72, 52);
+            this.btnChooseFiles.TabIndex = 5;
+            this.btnChooseFiles.Text = "Wybierz pliki";
+            this.btnChooseFiles.UseVisualStyleBackColor = true;
+            this.btnChooseFiles.Click += new System.EventHandler(this.btnChooseFiles_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(84, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Liczba wybranych plików: 0";
+            // 
+            // btnReplace
+            // 
+            this.btnReplace.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnReplace.Location = new System.Drawing.Point(6, 70);
+            this.btnReplace.Name = "btnReplace";
+            this.btnReplace.Size = new System.Drawing.Size(275, 45);
+            this.btnReplace.TabIndex = 7;
+            this.btnReplace.Text = "Nadpisz pliki";
+            this.btnReplace.UseVisualStyleBackColor = true;
+            this.btnReplace.Click += new System.EventHandler(this.btnReplace_Click);
+            // 
+            // FormDesktop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(299, 270);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FormDesktop";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FileClealer";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.ComboBox cbListToDelete;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog oFD1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnChooseFiles;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnReplace;
+    }
+}
+
---
+++ FileClealer/FormDesktop.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace FileClealer
+{
+    public partial class FormDesktop : Form
+    {
+        private List<string> itemsToClear = new List<string>();
+        private List<string> selectedFiles = new List<string>();
+        private string settingsFilePath = "settings.json";
+
+        public FormDesktop()
+        {
+            InitializeComponent();
+            StartSettings();
+        }
+
+        private void StartSettings()
+        {
+            LoadSettings(); // Wczytaj ustawienia przy starcie aplikacji
+            cbListToDelete.Items.AddRange(itemsToClear.ToArray());
+        }
+
+        private void LoadSettings()
+        {
+            // Sprawdź, czy plik z ustawieniami istnieje
+            if (File.Exists(settingsFilePath))
+            {
+                // Odczytaj dane z pliku JSON
+                string json = File.ReadAllText(settingsFilePath);
+                itemsToClear = JsonConvert.DeserializeObject<List<string>>(json);
+            }
+            else
+            {
+                // Jeśli plik nie istnieje, dodaj domyślne elementy do listy
+                itemsToClear.Add("<i>");
+                itemsToClear.Add("</i>");
+                itemsToClear.Add(@"{\an1}");
+                itemsToClear.Add(@"{\an2}");
+                itemsToClear.Add(@"{\an3}");
+                itemsToClear.Add(@"{\an4}");
+                itemsToClear.Add(@"{\an5}");
+                itemsToClear.Add(@"{\an6}");
+                itemsToClear.Add(@"{\an7}");
+                itemsToClear.Add(@"{\an8}");
+                itemsToClear.Add(@"{\an9}");
+            }
+        }
+
+        private void SaveSettings()
+        {
+            // Serializuj listę do formatu JSON
+            string json = JsonConvert.SerializeObject(itemsToClear);
+
+            // Zapisz dane do pliku
+            File.WriteAllText(settingsFilePath, json);
+        }
+
+        private void btnAddItem_Click(object sender, EventArgs e)
+        {
+            FormAddItem aI = new FormAddItem();
+            aI.FormClosed += onAdI_Close;
+            aI.ShowDialog();
+        }
+
+        private void onAdI_Close(object sender, FormClosedEventArgs e)
+        {
+            FormAddItem aI = (FormAddItem)sender;
+            if (string.IsNullOrEmpty(aI.itemText)) return;
+            if (itemsToClear.Contains(aI.itemText))
+            {
+                MessageBox.Show("Istnieje już taki element! Nie zapisano!");
+                return;
+            }
+
+            itemsToClear.Add(aI.itemText);
+            cbListToDelete.Items.Add(aI.itemText);
+
+            SaveSettings();
+            aI.Dispose();
+        }
+
+        private void btnRemoveItem_Click(object sender, EventArgs e)
+        {
+            if (cbListToDelete.SelectedItem != null)
+            {
+                string selectedItem = cbListToDelete.SelectedItem.ToString();
+
+                // Usuń z listy i z ComboBox
+                itemsToClear.Remove(selectedItem);
+                cbListToDelete.Items.Remove(selectedItem);
+
+                // Zapisz zmienione ustawienia
+                SaveSettings();
+            }
+        }
+
+
+        private void btnChooseFiles_Click(object sender, EventArgs e)
+        {
+            oFD1.InitialDirectory = Path.Combine(Environment.GetEnvironmentVariable("USERPROFILE"), "Desktop");
+            oFD1.Filter = "Pliki .srt|*.srt";
+
+            if (oFD1.ShowDialog() == DialogResult.OK)
+            {
+                selectedFiles.AddRange(oFD1.FileNames);
+                label1.Text = $"Liczba wybranych plików: {selectedFiles.Count}";
+            }
+        }
+
+        private void btnReplace_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                foreach (string filePath in selectedFiles)
+                {
+                    if (File.Exists(filePath))
+                    {
+                        string content = File.ReadAllText(filePath);
+                        foreach (string iTC in itemsToClear)
+                        {
+                            content = content.Replace(iTC, "");
+                        }
+                        File.WriteAllText(filePath, content);
+                    }
+                }
+                selectedFiles.Clear();
+                MessageBox.Show("Nadpisano!");
+
+                label1.Text = $"Liczba wybranych plików: {selectedFiles.Count}";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Coś nie tak. Podeślij mię to info: \n {ex}", "Coś nie działa!");
+            }
+
+        }
+    }
+}
